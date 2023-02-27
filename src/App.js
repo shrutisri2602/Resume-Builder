08315aac5d72a68dceac893de8343c8dd86b51eb
shruti srivastava <shruti.sri2602@gmail.com>
2023-02-27 23:28:09 +0530

    resume-builder

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Body from "./components/Body/Body";
+
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Body />
+    </div>
+  );
+}
+
+export default App;
